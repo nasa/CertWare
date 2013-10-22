554e200ab839d9065a99110efcd44e5b38cf99b4
Matt Barry <mrb@certware.net>
2013-10-22 16:42:02 -0500

    Tycho build configuration, product and update site
    Former-commit-id: 3402c95955794b82d294e3a22331734f3be7e0d7

---
+++ net.certware.product/target/products/net.certware.client/macosx/cocoa/x86_64/plugins/org.eclipse.epsilon.erl.engine_1.1.0.201309101707/src/org/eclipse/epsilon/erl/strategy/IEquivalentProvider.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dimitrios Kolovos - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.erl.strategy;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.eol.execute.context.IEolContext;
+
+public interface IEquivalentProvider {
+	
+	public Object getEquivalent(Collection collection, IEolContext context, List<String> rules) throws EolRuntimeException;
+	
+	public Object getEquivalent(Object source, IEolContext context, List<String> rules) throws EolRuntimeException;
+	
+	public Collection getEquivalents(Collection source, IEolContext context, List<String> rules) throws EolRuntimeException;
+	
+	public Collection getEquivalents(Object source, IEolContext context, List<String> rules) throws EolRuntimeException;
+	
+}
